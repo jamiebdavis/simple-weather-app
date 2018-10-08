cd9f88906b1a68887c26590ef3341005489acdda
Jamie Davis <jamied1985@gmail.com>
2018-10-08 14:24:07 +0100

    moved images to seperate folder

--- script.js
+++ script.js
@@ -29,21 +29,21 @@ function init(resultFromServer) {
   console.log(resultFromServer);
   switch (resultFromServer.weather[0].main) {
     case "Clear":
-      document.body.style.backgroundImage = `url("clear.jpg")`;
+      document.body.style.backgroundImage = `url("./images/clear.jpg")`;
       break;
     case "Clouds":
-      document.body.style.backgroundImage = `url("cloudy.jpg")`;
+      document.body.style.backgroundImage = `url("./images/cloudy.jpg")`;
       break;
     case "Rain":
     case "Drizzle":
     case "Mist":
-      document.body.style.backgroundImage = `url("rain.jpg")`;
+      document.body.style.backgroundImage = `url("./images/rain.jpg")`;
       break;
     case "Snow":
-      document.body.style.backgroundImage = `url("snow.jpg")`;
+      document.body.style.backgroundImage = `url("./images/snow.jpg")`;
       break;
     case "Thunderstorm":
-      document.body.style.backgroundImage = `url("storm.jpg")`;
+      document.body.style.backgroundImage = `url("./images/storm.jpg)`;
       break;
 
     default:
